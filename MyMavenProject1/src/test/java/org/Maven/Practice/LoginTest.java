552e099c6eda7ef71a1454d1b4be8d66e24366a9
narsimha78 <narsimha78@gmail.com>
2016-05-16 17:21:54 +1000

    closing driver after waiting for 5 seconds after page was loaded completely

--- MyMavenProject1/src/test/java/org/Maven/Practice/LoginTest.java
+++ MyMavenProject1/src/test/java/org/Maven/Practice/LoginTest.java
@@ -7,12 +7,15 @@ import org.testng.annotations.Test;
 public class LoginTest {
 	
 	@Test
-	public void LoginFuntion(){
+	public void LoginFuntion() throws InterruptedException{
 		
 		System.out.println("Login Fuction to access email account");
 		WebDriver driver=new FirefoxDriver();
 		driver.get("http://greatandhra.com");
 		//greatandhra website page should open 
+		
+		Thread.sleep(5000L);
+		driver.close();
 	}
 
 }
